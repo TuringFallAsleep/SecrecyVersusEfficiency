e1e427770547cae4affba9491487f56196e11a1d
TuringFallSleep <boyin.yang@student.manchester.ac.uk>
2018-02-08 02:06:49 +0000

    Renew the GUI

--- svse_tutorial/src/main/java/Model/Tutorial.java
+++ svse_tutorial/src/main/java/Model/Tutorial.java
@@ -2,10 +2,21 @@ package Model;
 
 import View.GUI;
 
+import View.GUI;
+
+import javax.swing.*;
+
 public class Tutorial {
     public static void main(String args[]) {
 
-        GUI window = new GUI();
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                UIManager.put("swing.boldMetal",Boolean.FALSE);
+                GUI.createAndShowGUI();
+            }
+        });
+//        GUI window = new GUI();
 //        window.setVisible(true);
 
         //test jFreeChart
